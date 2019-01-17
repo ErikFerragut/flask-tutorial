f9642b1ec2918dca8cced49280576b61d720bee1
Erik Ferragut <erikferragut@gmail.com>
2019-01-16 21:59:40 -0500

    moved sphinx docs to docs dir

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ release = ''
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.githubpages',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -65,7 +65,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,5 +1,5 @@
 .. Flask Tutorial documentation master file, created by
-   sphinx-quickstart on Wed Jan 16 21:42:11 2019.
+   sphinx-quickstart on Wed Jan 16 21:55:17 2019.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
